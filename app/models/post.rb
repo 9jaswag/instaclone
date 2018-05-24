efd95534576ffc6aaa328579064f2a65e1d4e00f
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 21:12:16 +0100

    - add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_attached_file :image
+
+  validates :caption, presence: true
+  validates_attachment :image, presence: true
+  do_not_validate_attachment_file_type :image
 end
