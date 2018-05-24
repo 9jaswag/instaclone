5c3b8bbda81bc189762a246aa0b16aa55c0ca7e8
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 23:32:27 +0100

    - add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 
   def new
