b8d67f1a90401e13d9c34a8abd74b301d0715881
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 22:52:02 +0100

    - realtime posts with pusher

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_attached_file :image
+  after_create :notify_pusher
 
   validates :caption, presence: true
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }, size: { in: 0..500.kilobytes }
+
+  def serialised
+    {
+      id: self.id,
+      caption: self.caption,
+      user: self.user.username,
+      image: self.image.url
+    }
+  end
+
+  def notify_pusher
+    Pusher.trigger('post', 'new', self.serialised)
+  end
 end
